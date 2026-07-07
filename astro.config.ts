d91e72450753c80163ef95c6b774f1996c6cce94
hammtah <hammtah@github.com>
2026-07-07 22:02:11 +0100

    Enhance markdown configuration and update post styles
    - Updated markdown configuration to support light and dark themes using GitHub styles.
    - Improved styles for code blocks in journal prose, including new classes for copy code wrappers and dark mode adjustments.

--- astro.config.ts
+++ astro.config.ts
@@ -27,7 +27,10 @@ export default defineConfig({
   },
   markdown: {
     shikiConfig: {
-      theme: 'css-variables',
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark'
+      },
       wrap: false
     },
     remarkPlugins: [
